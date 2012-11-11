a8f46ebc07a218789bf6a99cb01aa2d63fffb7db
Will <willandmeling@gmail.com>
2012-11-10 22:37:31 -0800

    removed unused code and fixed q/qu bug

--- boggle_solver.cpp
+++ boggle_solver.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 
 const int boarder = 1; //create a border of null CUBES so we know when we reach an edge
-const int COLS = 100 + (boarder*2);
+const int COLS = 4 + (boarder*2);
 const int BOARD_SIZE = COLS * COLS;
 const int NUM_NEIGHBORS = 8;
 const int MAX_INPUT = 50; //longest word in dictionary
@@ -32,7 +32,6 @@ map <string,bool> words; //found words
 const int NUM_GROUPS = 11;
 const string LETTER_GROUPS[NUM_GROUPS] = {"e","t","ar","ino","s","d","chl","fmpu","gy","w","bjkqvxz"};
 const int LETTER_GROUP_FREQ[NUM_GROUPS] = {19,13,12,11,9,6,5,4,3,2,1};
-int count = 0;
 
 void init(){
     string group;
@@ -70,24 +69,6 @@ void printboard(){
     cout << endl;
 }
 
-void printNeighbors( int x ){
-    char neighbor;
-    int nIndex;
-
-    if( board[x] == '*' ){
-        cout << "Can't print neighbors of border piece";
-        return;
-    }
-
-    for(int i = 0; i < NUM_NEIGHBORS; i++){
-        nIndex = NEIGHBORS[i] + x;
-        neighbor = board[nIndex];
-        cout << neighbor << ' ';
-        if( i == 3 ) cout << board[x] << ' ';
-        if( (i == 2) || (i == 4) ) cout << endl;     
-    }
-}
-
 // load dictionary into hash table
 void buildDict()
 {
@@ -95,34 +76,29 @@ void buildDict()
     //http://www.cs.duke.edu/courses/cps100/spring05/assign/boggle/code/bogwords.txt
     in_file.open("bogwords.txt");
     string word;
-    // string prefix;
-    int i, j = 0;
+    string prefix;
+    int i;
     int len;
-    //extra paragraph returns in dictionary will break this
+
     while( getline(in_file,word) )
     {
         len = word.length();
         dict[word] = true;
 
 
-        string prefix = "";
+        prefix = "";
         for(i = 0; i < len; i++){
             prefix += word[i];
             prefixes[prefix] = true;
         }
-        // if( dict["blah"] == 0 ) cout << "not found" << endl;
-        // if( j > 1 ) break;
-
-        // j++;
     }
     cout << "words: " << dict.size() << endl;
-    // cout << "prefixes: " << prefixes.size() << endl;
 }
 
-// i = board index
 // searched = used cubes on the board
 void find(int i, string str, vector<bool> searched){
     str += board[i];
+    if(board[i] == 'q') str += 'u';
     if( searched[i] || (board[i] == '*') || !prefixes[str]   ) return; //|| (prefixes[str] == 0)
     
     if( dict[str] ) words[str] = true;
@@ -142,20 +118,19 @@ int main(){
     for(int i = 0; i < BOARD_SIZE; i++) searched.push_back(false);
 
     init();
-    // printboard();
-    // printNeighbors( 6 );
+    buildDict();
 
-   buildDict();
-
-   clock_t begin = clock();
-   for(int i = 0; i < BOARD_SIZE; i++){
-       find(i, str, searched);
-   }
-   cout << "Words found: " << words.size() << endl;
-   cout << double(clock() - begin) / CLOCKS_PER_SEC << " seconds elapsed" << endl;
-   // cout << "Prefixes found: " << count << endl;
-   map<string, bool>::iterator p;
-   // for(p = words.begin(); p != words.end(); p++) {
-   //     cout << p->first << endl;
-   //   }
+    clock_t begin = clock();
+    for(int i = 0; i < BOARD_SIZE; i++){
+        find(i, str, searched);
+    }
+    printboard();
+    cout << "Words found: " << words.size() << endl;
+    cout << double(clock() - begin) / CLOCKS_PER_SEC << " seconds to search" << endl;
+    
+    //print words
+    // map<string, bool>::iterator p;
+    // for(p = words.begin(); p != words.end(); p++) {
+    //     cout << p->first << endl;
+    // }
 }
